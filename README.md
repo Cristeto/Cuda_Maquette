3d3ca3be2948e2b5aae31256b11570cd6d2eb40b
Cristeto <s.espinamagdalena@codeur-online>
2019-09-19 16:46:37 +0200

    Integration maquette Cuda

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cuda_Single_Page_Portfolio
+Integration of « Cuda Single Page Portfolio » web template.
+![Cude Single Page Portfolio](https://graphicburger.com/wp-content/uploads/2013/06/Cuda-Single-Page-Portfolio-600.png)
---
+++ script.js
@@ -0,0 +1,35 @@
+/* >>>>>>>>>> SMOOTH SCROLL <<<<<<<<<< */
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+/* >>>>>>>>>> PROGRESS BAR <<<<<<<<<< */
+
+$(function() {
+  $(".progress").each(function() {
+    var value = $(this).attr('data-value');
+    var left = $(this).find('.progress-left .progress-bar');
+    var right = $(this).find('.progress-right .progress-bar');
+    if (value > 0) {
+      if (value <= 50) {
+        right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
+      } else {
+        right.css('transform', 'rotate(180deg)')
+        left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+      }
+    }
+  })
+  function percentageToDegrees(percentage) {
+    return percentage / 100 * 360
+  }
+});
